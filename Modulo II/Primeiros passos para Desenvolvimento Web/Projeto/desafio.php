b92bc06ff3e1b044ca7d1978e9f755e67303c8b3
Victor <victormazzofilho@outlook.com>
2022-04-26 16:51:40 -0300

    Adicionamos os evercícios dos módulos 'Primeiros passos para desenvolvimento web', 'Introdução a criação de websites com HTML5 e CSS3' e seus respectivos certificados.

---
+++ Modulo II/Primeiros passos para Desenvolvimento Web/Projeto/desafio.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Desafio DIO - Construindo sua primeira aplicação em PHP</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
+</head>
+<body>
+ 
+<div id="accordion">
+  <h3>Sport Club Corinthians Paulista</h3>
+  <div>
+    <p>
+    Sport Club Corinthians Paulista é um clube poliesportivo brasileiro da cidade de São Paulo, capital do estado de São Paulo. Foi fundado como uma equipe de futebol no dia 1 de setembro de 1910 por um grupo de operários do bairro Bom Retiro. Seu nome foi inspirado no Corinthian FC de Londres, que excursionava pelo Brasil.
+    </p>
+  </div>
+  <h3>Sociedade Esportiva Palmeiras</h3>
+  <div>
+    <p>
+    Sociedade Esportiva Palmeiras (pronúncia em português: [sosiedˈadʒi ispoɾtʃˈivə pawmˈejɾəs], conhecida popularmente como Palmeiras) é um clube poliesportivo brasileiro da cidade de São Paulo, capital do estado homônimo. Foi fundado em 26 de agosto de 1914 e suas cores, presentes no escudo e bandeira oficial, são o verde e branco.
+    </p>
+  </div>
+  <h3>Santos Futebol Clube</h3>
+  <div>
+    <p>
+    Santos Futebol Clube é um clube poliesportivo brasileiro da cidade de Santos, São Paulo. É Também a equipe com mais gols na história do futebol. Foi fundado em 14 de abril de 1912, suas cores iniciais seriam o branco, azul e dourado, mas um ano após a sua fundação, ficou decidido que as cores do clube passariam a ser branco e preto.
+    </p>
+  </div>
+  <h3>São Paulo Futebol Clube</h3>
+  <div>
+    <p>
+    São Paulo Futebol Clube é um clube poliesportivo brasileiro da cidade de São Paulo, capital do estado homônimo. Foi fundado em 25 de janeiro de 1930, tendo interrompido suas atividades em maio de 1935, e as retomado em dezembro do mesmo ano.
+    </p>
+  </div>
+</div>
+ 
+ 
+</body>
+</html>
